ecca271dd9ef86c98e4a9be07802c62678e4f786
Vlad <vlad-labytin@yandex.ru>
2025-01-12 13:03:28 +0300

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -11,6 +11,3 @@ def sort_by_date(records: list[dict], descending: [bool] = True) -> list[dict]:
     """Сортирует список словарей по ключу date"""
     sort_records = sorted(records, key=lambda record: record.get("date"), reverse=descending)
     return sort_records
-
-
-
