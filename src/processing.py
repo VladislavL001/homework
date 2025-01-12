197f4b77a7eec2672e37fc69b0b5ceab4e43d539
Vlad <vlad-labytin@yandex.ru>
2025-01-12 12:58:19 +0300

    add def sord by date

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,25 @@
-def filter_by_state(records: list[dict], state: [str]="EXECUTED") -> list[dict]:
+def filter_by_state(records: list[dict], state: [str] = "EXECUTED") -> list[dict]:
     """Функция возвращает словари у которых ключ соответсвует state"""
     filtered_records = []
     for record in records:
-        if record.get ('state') == state:
+        if record.get("state") == state:
             filtered_records.append(record)
     return filtered_records
 
 
+def sort_by_date(records: list[dict], descending: [bool] = True) -> list[dict]:
+    """Сортирует список словарей по ключу date"""
+    sort_records = sorted(records, key=lambda record: record.get("date"), reverse=descending)
+    return sort_records
+
+
 print(
-    filter_by_state(
+    sort_by_date(
         [
             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         ]
     )
 )
