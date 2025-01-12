a68b343acbc6cbed43216323acf3ad1af7f87c3b
Vlad <vlad-labytin@yandex.ru>
2025-01-12 13:19:38 +0300

    add PEP-8

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(records: list[dict], state: [str] = "EXECUTED") -> list[dict]:
+def filter_by_state(records: list, state: str = "EXECUTED") -> list:
     """Функция возвращает словари у которых ключ соответсвует state"""
     filtered_records = []
     for record in records:
@@ -7,7 +7,7 @@ def filter_by_state(records: list[dict], state: [str] = "EXECUTED") -> list[dict
     return filtered_records
 
 
-def sort_by_date(records: list[dict], descending: [bool] = True) -> list[dict]:
+def sort_by_date(records: list, descending: bool = True) -> list:
     """Сортирует список словарей по ключу date"""
-    sort_records = sorted(records, key=lambda record: record.get("date"), reverse=descending)
+    sort_records = sorted(records, key=lambda record: record["date"], reverse=descending)
     return sort_records
