3d4886c6162f6655c652a0bb14084037b3dea857
Vlad <vlad-labytin@yandex.ru>
2025-01-30 14:35:21 +0300

    Add test_decorators, update PEP-8

--- src/decorators.py
+++ src/decorators.py
@@ -1,30 +1,33 @@
 from functools import wraps
-from typing import Callable, Any, Optional
+from typing import Any, Callable
 
-def log(filename: str="console") -> Callable:
+
+def log(filename: str = "console") -> Callable:
+    """Декоратор принимает функцию с параметром вывода и выдает логи использования"""
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             log_massage = ""
             try:
                 result = func(*args, **kwargs)
-                log_massage = f'{func.__name__} ok\n'
+                log_massage = f"{func.__name__} ok\n"
                 return result
-            except Exception  as error:
-                log_massage = f'{func.__name__} error: {type(error)}.\nInputs: {args}, {kwargs}'
+            except Exception as error:
+                log_massage = f"{func.__name__} error: {type(error)}.\nInputs: {args}, {kwargs}"
                 return ""
             finally:
                 if filename == "console":
-                    print (log_massage, end="\n")
+                    print(log_massage, end="")
                 else:
                     with open(filename, "w", encoding="utf-8") as f:
                         f.write(log_massage)
+
         return wrapper
     return decorator
 
 
-@log(filename="mylog.txt")
-def my_function(x: int, y:int) -> int:
-    return x + y
-
-print(my_function(1, 2))
+# @log (filename="mylog.txt")
+# def my_func (x,y):
+#     return x+y
+#
+# print (my_func(1,2))
